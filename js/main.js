f80609444483bcf0bb46e3fc1ae352e76c98a876
annalenadavis <annalenakdavis@gmail.com>
2018-01-12 11:44:10 -0500

    add updated files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
-    
+$(document).ready(function() {
+
+
+//Toggle the nav dropdown menu on tablets/phone
+$("#menuBtn").click(function(){
+  event.preventDefault();
+  $("#tabletNavDropdown").slideToggle();
+});
+
+
+//Play Video when mouse hovers over it, pause when mouse leaves
+const video=document.getElementById("video");
+const toggle = document.getElementById('.toggle');
+
+video.addEventListener('mouseover', function(){
+  this.play();
+});
+video.addEventListener('mouseout', function(){
+  this.pause();
+})
+
+//Video controls functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // ready method end
