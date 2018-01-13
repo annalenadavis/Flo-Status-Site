4e7267e515540ad1ca72e958ce1a85f145e84fcf
annalenadavis <annalenakdavis@gmail.com>
2018-01-13 15:22:17 -0500

    add updated files

--- js/main.js
+++ js/main.js
@@ -19,20 +19,68 @@ video.addEventListener('mouseout', function(){
   this.pause();
 })
 
-//Video controls functions
-function togglePlay() {
-  const method = video.paused ? 'play' : 'pause';
-  video[method]();
-}
 
-function updateButton() {
-  const icon = this.paused ? '►' : '❚ ❚';
-  toggle.textContent = icon;
-}
 
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Video sound or mute player_controls
+// $("#video").prop('muted', true);
+//
+// $("#soundBtn").click(function () {
+//     if ($("#video").prop('muted')) {
+//         $("#video").prop('muted', false);
+//         $(this).addClass('unmute-video');
+//
+//     } else {
+//         $("#video").prop('muted', true);
+//         $(this).removeClass('unmute-video');
+//     }
+//     console.log($("#video").prop('muted'))
+// });
+
+// const soundBtn = document.getElementById('soundBtn');
+//     soundBtn.onclick = function (){
+//     if (video.muted === false) {
+//            video.muted = true;
+//     }
+//     else {
+//         video.muted = false;
+//     }
+//     };
+
+
+//Play button functions
+// function togglePlay() {
+//   const method = video.paused ? 'play' : 'pause';
+//   video[method]();
+// }
+
+// function updateButton() {
+//   const icon = this.paused ? '►' : '❚ ❚';
+//   toggle.textContent = icon;
+// }
+
+// video.addEventListener('click', togglePlay);
+// video.addEventListener('play', updateButton);
+// video.addEventListener('pause', updateButton);
 
 
 
