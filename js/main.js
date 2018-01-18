db3009a818d3f3c5861c08527ec5720eb9e24849
Annalena Davis <annalenakdavis@gmail.com>
2018-01-18 08:43:26 -0500

    add updated files

--- js/main.js
+++ js/main.js
@@ -1,42 +1,59 @@
 $(document).ready(function() {
 
 
-//Toggle the nav dropdown menu on tablets/phone
+
+//Toggle the nav dropdown menu on tablets/phones with menu button
 $("#menuBtn").click(function(){
   event.preventDefault();
   $("#tabletNavDropdown").slideToggle();
+  $(".tablet_nav_dropdown_background").slideToggle();
+});
+
+//Need code to close dropdown when a link is clicked
+
+//Toggle nav dropdown when clicking outside menu
+$(".tablet_nav_dropdown_background").click(function(){
+  $("#tabletNavDropdown").slideToggle();
+  $(".tablet_nav_dropdown_background").slideToggle();
 });
 
 
-//Play Video when mouse hovers over it, pause when mouse leaves
-const video2=document.getElementById("video2");
+//Scroll to Sign-up form when "Start Today" is clicked
+$(".start_today").click(function(event){
+  event.preventDefault();
+  const slideTarget = $(".start_today_header");
+  $("html,body").animate({
+      "scrollTop" : $(slideTarget).offset().top + "px"
+  });//animate method end
+}); //click method end
 
-video2.addEventListener('mouseover', function(){
-  this.play();
-});
-video2.addEventListener('mouseout', function(){
-  this.pause();
-})
-
-//Remove controls in desktop mode (need for tablet/phone since hover won't work)
-if (screen.width >= 992) {
-video2.controls = false;
-}
-
-//Play Video when mouse hovers over it, pause when mouse leaves
+//Play First Video when mouse hovers over it, pause when mouse leaves
 const video1=document.getElementById("video1");
-
 video1.addEventListener('mouseover', function(){
   this.play();
 });
 video1.addEventListener('mouseout', function(){
   this.pause();
 })
-
 //Remove controls in desktop mode (need for tablet/phone since hover won't work)
 if (screen.width >= 992) {
 video1.controls = false;
 }
+
+//Play Second Video when mouse hovers over it, pause when mouse leaves
+const video2=document.getElementById("video2");
+video2.addEventListener('mouseover', function(){
+  this.play();
+});
+video2.addEventListener('mouseout', function(){
+  this.pause();
+})
+//Remove controls in desktop mode 
+//(need for tablet/phone since hover won't work)
+if (screen.width >= 992) {
+video2.controls = false;
+}
+
 
 
 
@@ -84,19 +101,6 @@ video1.controls = false;
 // video.addEventListener('click', togglePlay);
 // video.addEventListener('play', updateButton);
 // video.addEventListener('pause', updateButton);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
